f674acbfc18e83be3e5c6adf360e6c7b3d07fcf8
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-05-08 22:57:03 +0300

    test(app): add cubit unit tests and test dependencies

---
+++ test/cubit/notifications_cubit_test.dart
@@ -0,0 +1,79 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fluidity/cubit/notifications/notifications_cubit.dart';
+import 'package:fluidity/models/reminder_setting.dart';
+
+class _FakeNotificationGateway implements NotificationGateway {
+  bool initCalled = false;
+  bool requestCalled = false;
+  bool cancelAllCalled = false;
+  int syncCalls = 0;
+  bool throwOnPermission = false;
+
+  @override
+  Future<void> init() async {
+    initCalled = true;
+  }
+
+  @override
+  Future<void> requestPermissions() async {
+    requestCalled = true;
+    if (throwOnPermission) {
+      throw Exception('permission denied');
+    }
+  }
+
+  @override
+  Future<void> cancelAll() async {
+    cancelAllCalled = true;
+  }
+
+  @override
+  Future<void> sync(List<ReminderSetting> reminders) async {
+    syncCalls += 1;
+  }
+
+  @override
+  void updateLocalizedStrings(dynamic loc) {}
+}
+
+void main() {
+  late _FakeNotificationGateway gateway;
+  late NotificationsCubit cubit;
+
+  setUp(() {
+    gateway = _FakeNotificationGateway();
+    cubit = NotificationsCubit(notificationService: gateway);
+  });
+
+  tearDown(() async {
+    await cubit.close();
+  });
+
+  test('bootstrap enables notifications when permission request succeeds', () async {
+    await cubit.bootstrap();
+
+    expect(gateway.initCalled, isTrue);
+    expect(gateway.requestCalled, isTrue);
+    expect(cubit.state.systemAllowed, isTrue);
+    expect(cubit.state.enabled, isTrue);
+  });
+
+  test('bootstrap disables notifications when permission request fails', () async {
+    gateway.throwOnPermission = true;
+
+    await cubit.bootstrap();
+
+    expect(cubit.state.systemAllowed, isFalse);
+    expect(cubit.state.enabled, isFalse);
+    expect(gateway.cancelAllCalled, isTrue);
+  });
+
+  test('setEnabled(false) cancels all notifications', () async {
+    await cubit.bootstrap();
+    await cubit.setEnabled(false, const []);
+
+    expect(gateway.cancelAllCalled, isTrue);
+    expect(cubit.state.enabled, isFalse);
+  });
+}
