53af4efa61ea4500b65b9fe3654aa25bc5de6572
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-05-19 23:09:11 +0300

    docs(utils): add import-style comment to time_format.dart

--- lib/utils/time_format.dart
+++ lib/utils/time_format.dart
@@ -1,3 +1,5 @@
+// Prefer using relative imports for this utility (e.g. `../utils/time_format.dart`) to
+// avoid duplicate library instances caused by mixing `package:` and relative URIs.
 String formatHm(DateTime dt) {
   final hh = dt.hour.toString().padLeft(2, '0');
   final mm = dt.minute.toString().padLeft(2, '0');
