d526de683b68f02fd403a26d1cec43ea4586bcf0
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-05-22 09:25:58 +0300

    test(utils): expand formatHm coverage

--- test/utils/time_format_test.dart
+++ test/utils/time_format_test.dart
@@ -6,4 +6,14 @@ void main() {
     final dt = DateTime(2020, 1, 1, 9, 5);
     expect(formatHm(dt), '09:05');
   });
+
+  test('formatHm pads zeros at midnight', () {
+    final dt = DateTime(2020, 1, 1, 0, 0);
+    expect(formatHm(dt), '00:00');
+  });
+
+  test('formatHm handles end of day', () {
+    final dt = DateTime(2020, 1, 1, 23, 59);
+    expect(formatHm(dt), '23:59');
+  });
 }
